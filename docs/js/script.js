bccb161f2c50b4274b0a6603022d34465d0e1ddb
mahdihassani9374 <mahdihassani9374@gmail.com>
2020-04-23 18:29:31 +0430

    slick \

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,31 @@
 $(function () {
   $('#bootstrap-touch-slider').bsTouchSlider();
+
+  $('.project__slick').slick({
+    lazyLoad: 'ondemand',
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 })
