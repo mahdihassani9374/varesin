643462e0231eb70839a7a1d07710b2dc707d1417
Mahdi Hasani <mahdi.hasani@chargoon.net>
2020-04-27 15:22:10 +0330

    swal , toaster

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,24 @@
 $(function () {
-  $('#bootstrap-touch-slider').bsTouchSlider()
+  $('#bootstrap-touch-slider').bsTouchSlider();
+
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "progressBar": true,
+    "positionClass": "toast-bottom-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "timeOut": "5000",
+  }
+
+  setTimeout(function () {
+     toastr.success('سلام خوش آمدید ');
+  }, 2000);
+
+  Swal.fire({
+    icon: 'success',
+    title: 'تبریک',
+    text: 'عملیات با موفقیت انجام شد ',  
+    confirmButtonText:'بستن'  
+  })
 })
