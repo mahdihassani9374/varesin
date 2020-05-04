d00fd2ee0ae87907724d39022ffc86711ecda139
mahdihassani9374 <mahdihassani9374@gmail.com>
2020-05-04 14:04:00 +0430

    slick

--- docs/js/script.js
+++ docs/js/script.js
@@ -11,14 +11,49 @@ $(function () {
     "timeOut": "5000",
   }
 
-  setTimeout(function () {
-     toastr.success('سلام خوش آمدید ');
-  }, 2000);
+  // setTimeout(function () {
+  //    toastr.success('سلام خوش آمدید ');
+  // }, 2000);
 
-  Swal.fire({
-    icon: 'success',
-    title: 'تبریک',
-    text: 'عملیات با موفقیت انجام شد ',  
-    confirmButtonText:'بستن'  
+  // Swal.fire({
+  //   icon: 'success',
+  //   title: 'تبریک',
+  //   text: 'عملیات با موفقیت انجام شد ',  
+  //   confirmButtonText:'بستن'  
+  // })
+
+  $('.project__slick').slick({
+    infinite:false,
+    centerMode: false,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('[data-role="slick-left"]').on('click',function(){
+    $('.project__slick').slick('slickPrev');
+  })
+  $('[data-role="slick-right"]').on('click',function(){
+    $('.project__slick').slick('slickNext');
   })
 })
